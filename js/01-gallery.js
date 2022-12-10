254a5bf44ac5f8d418c4e2f465d9b0ff0826ed58
VictoriaSzn <victoriia.szn@gmail.com>
2022-12-10 16:05:50 +0200

    перше читання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -24,64 +24,35 @@ function CreateMarkup(cards) {
     `;
     }).join('');
 }
-//действие вызов модалки
+// вызов модалки + Esc
 
 galerylistEl.addEventListener('click', onModalWin)
 
 function onModalWin(evt) {
-    // if (!evt.target.classList.contains('.gallery__link')) {
+   
+    evt.preventDefault();
+ // if (!evt.target.classList.contains('.gallery__link')) {
     //     return;
     // }
-    evt.preventDefault();
-
-//if (evt.target.nodeName !== 'IMG') { return; }
-    // const carousel = basicLightbox.create(` 
-    //    <img src="${evt.target.dataset.source}"
-    //     width="800" height="600">  `,
-    //     {
-    //         onShow: carousel => {
-    //             document.addEventListener('keydown', onEscapeKeyDown);
-    //         }, onClose: carousel => {
-    //             document.removeEventListener('keydown', onEscapeKeyDown);
-    //         },
-    //     }); carousel.show();
-    // function onEscapeKeyDown(event) {
-    //     if (event.code !== 'Escape') { return; } carousel.close();
-    // }
-    
-
-
-	const carousel = basicLightbox.create(`
+    const carousel = basicLightbox.create(`
 		<img src="${evt.target.dataset.source}"
-        width="800" height="600"> >
-	`).show()
-
-
+        width="800" height="600"> `,
+        {
+            onShow: carousel => {
+                document.addEventListener('keydown', onEscapeKeyDown);
+            },
+            onClose: carousel => {
+                document.removeEventListener('keydown', onEscapeKeyDown);
+            },
+        }
+    );
+    carousel.show();
     
-    // const currentActiveImg = document.querySelector('.gallery__item.modal_open')
-    // if (currentActiveImg) {
-    //     currentActiveImg.classList.remove('modal');
-    // }
-    // const currentImg = evt.target;
-    // const parentCurrentImg = currentImg.closest('.gallery__item');
-    //     parentCurrentImg.classList.add('modal');
-
-    // console.log(parentCurrentImg);
-    // console.log(evt.currentTarget.dataset.description);
+    function onEscapeKeyDown(evt) {
+        if (evt.code === 'Escape') {
+            carousel.close();
+        }
+    }
 }
 
-
-console.log(galleryItems);
-
-// import * as basicLightbox from 'basiclightbox'
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// `)
-
-//instance.show()
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,12 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryDivEl = document.querySelector('.gallery');
+const cardsMarkup = CreateMarkup(galleryItems);
+galleryDivEl.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function CreateMarkup (cards) {
+    return cards
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+            `;
+    }).join('');
+}
+
+galleryDivEl.addEventListener('click', onModalWin);
+
+function onModalWin(evt) {
+    evt.preventDefault();
+
+    let gallery = new SimpleLightbox('.gallery__item', {
+        animationSlide: false,
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+    
+   
+}
 
 
-
-
-
-console.log(galleryItems);
 
 
 
